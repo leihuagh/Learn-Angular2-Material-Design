96e71691f9f2769addebc305e063dff57b55aa88
Lei Hua <lhua@masslottery.com>
2016-11-02 08:28:45 -0400

    add material design core

---
+++ 03-buttons/src/app/lib/core/a11y/index.ts
@@ -0,0 +1,23 @@
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {FocusTrap} from './focus-trap';
+import {MdLiveAnnouncer} from './live-announcer';
+import {InteractivityChecker} from './interactivity-checker';
+
+export {FocusTrap} from './focus-trap';
+export {MdLiveAnnouncer} from './live-announcer';
+export {InteractivityChecker} from './interactivity-checker';
+
+export const A11Y_PROVIDERS = [MdLiveAnnouncer, InteractivityChecker];
+
+@NgModule({
+  declarations: [FocusTrap],
+  exports: [FocusTrap],
+})
+export class A11yModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: A11yModule,
+      providers: A11Y_PROVIDERS,
+    };
+  }
+}
