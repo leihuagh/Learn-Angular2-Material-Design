f5b56c62798a846e8dda02b6b00ab586a35c408a
Lei Hua <lhua@masslottery.com>
2016-11-02 07:27:48 -0400

    initialization

---
+++ 01-starter/src/vendor.ts
@@ -0,0 +1,16 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+// RxJS
+import 'rxjs';
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'animate.css/animate.css';
+import 'normalize.css/normalize.css';
---
+++ README.md
@@ -0,0 +1,1 @@
+# Learn process to make angular 2 based material design lib
