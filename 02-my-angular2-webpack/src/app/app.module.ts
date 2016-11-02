90a2877457186e12252de3a5f28a522db52e396b
Lei Hua <lhua@masslottery.com>
2016-11-02 08:12:20 -0400

    add my angular2-webpack starter

--- 02-my-angular2-webpack/src/app/app.module.ts
+++ 02-my-angular2-webpack/src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { CarModule } from './car/car.module';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    FormsModule,
-    HomeModule,
-    CarModule
+    BrowserModule
   ],
   declarations: [
     AppComponent
--- 02-my-angular2-webpack/src/app/home/motion/motion.model.ts
+++
@@ -1,7 +0,0 @@
-export class Motion {
-    _id: string;
-    type: string;
-    category: string;
-    date: Date;
-    amount: number;
-}
